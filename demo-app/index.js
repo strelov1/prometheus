5ffbbb435846b69739da5288740ca65cad70e2ee
Sean McBride <spmcbride1201@gmail.com>
2018-08-24 16:56:24 -0400

    feat: instrumenting node.js app

--- demo-app/index.js
+++ demo-app/index.js
@@ -1,23 +1,35 @@
-const express = require("express");
-const app = express();
-
-app.get("/", (req, res) => res.send("Hello World!"));
-app.get("/decipher", (req, res) => res.send("Hello Decipher!"));
-
-app.listen(1337, () =>
-  console.log("Server listening to 1337, metrics exposed on /metrics endpoint")
-);
-
 const client = require("prom-client");
+const express = require("express");
+
+const app = express();
 
 const collectDefaultMetrics = client.collectDefaultMetrics;
 const Registry = client.Registry;
 
 const register = new Registry();
+// Probe every 5th second.
+collectDefaultMetrics({ register, timeout: 5000 });
+
+const httpRequestsTotalCounter = new client.Counter({
+  name: "http_requests_total",
+  help: "metric_help",
+  labelNames: ["path", "method"],
+  registers: [register]
+});
+
+app.get("/", (req, res) => {
+  httpRequestsTotalCounter.labels("/", "GET").inc();
+  return res.send("Hello World!");
+});
+app.get("/decipher", (req, res) => {
+  httpRequestsTotalCounter.labels("/decipher", "GET").inc();
+  return res.send("Hello Decipher!");
+});
 app.get("/metrics", (req, res) => {
   res.set("Content-Type", register.contentType);
   res.end(register.metrics());
 });
 
-// Probe every 5th second.
-collectDefaultMetrics({ register, timeout: 5000 });
+app.listen(1337, () =>
+  console.log("Server listening to 1337, metrics exposed on /metrics endpoint")
+);
